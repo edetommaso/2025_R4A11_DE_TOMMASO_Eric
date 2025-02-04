01ef3f108a296fd6347f642c31efb1b81f8d1dca
edetommaso <eric.detommaso@gmail.com>
2025-02-04 16:53:04 +0100

    tp fini for real

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tp2/main.dart';
+import 'package:counter_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
